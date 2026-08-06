959fcf8ac7eded89abeccca33883b656a0161e1e
utkashu281 <utkarshu888@gmail.com>
2026-08-06 21:41:25 +0530

    recursive insertionsort

---
+++ src/Algorithm/Sorting/SortingTwo/RecursiveInsertionSort/InsertionSort.java
@@ -0,0 +1,8 @@
+package Algorithm.Sorting.SortingTwo.RecursiveInsertionSort;
+
+public class MyinsertionSort {
+    public static void main(String[] args) {
+        int[] arr={10,4,43,5,57,91,45,9,7};
+    }
+    public static int[] myInsertionSort(){}
+}
